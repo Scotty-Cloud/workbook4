6ad5ed5c0420e652892f9ae624bfd58d29e623e6
scott <scottysora@gmail.com>
2024-05-07 11:11:29 -0400

    separate different loops into respective files

--- exercises/loopScripts/while-loops.js
+++ exercises/loopScripts/while-loops.js
@@ -4,15 +4,3 @@ while (count <= 7) {
   console.log("I love loops");
   count++;
 }
-
-//*for loop
-for (let i = 0; i < 7; i++) {
-  console.log("i love loops too");
-}
-
-//*do-while loop
-let i = 1;
-do {
-  console.log("i love loops aswell");
-  i++;
-} while (i <= 7);
