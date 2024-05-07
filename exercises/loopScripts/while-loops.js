5f98469452ac86f05343e13252a87bb2d70d6575
scott <scottysora@gmail.com>
2024-05-07 09:48:33 -0400

    add for loop and do-while loop exercise

--- exercises/loopScripts/while-loops.js
+++ exercises/loopScripts/while-loops.js
@@ -1,5 +1,18 @@
+//*while loop
 let count = 1;
 while (count <= 7) {
   console.log("I love loops");
   count++;
 }
+
+//*for loop
+for (let i = 0; i < 7; i++) {
+  console.log("i love loops too");
+}
+
+//*do-while loop
+let i = 1;
+do {
+  console.log("i love loops");
+  i++;
+} while (i <= 7);
