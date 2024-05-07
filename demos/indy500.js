e53a17f332b232ba00d9757316a5088209f3a48c
scott <scottysora@gmail.com>
2024-05-07 10:16:25 -0400

    add loop example for speed

---
+++ demos/indy500.js
@@ -0,0 +1,7 @@
+let lap = 1;
+
+
+while(lap <= 500) {
+    console.log("Car finished is on lap " + lap)
+    lap++;
+}
--- exercises/loopScripts/while-loops.js
+++ exercises/loopScripts/while-loops.js
@@ -13,6 +13,6 @@ for (let i = 0; i < 7; i++) {
 //*do-while loop
 let i = 1;
 do {
-  console.log("i love loops");
+  console.log("i love loops aswell");
   i++;
 } while (i <= 7);
