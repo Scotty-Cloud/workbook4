a0aff0625b13c5b58d6276afcd68109d93a47b60
scott <scottysora@gmail.com>
2024-05-08 13:48:25 -0400

    complete finding vehicle color & fix syntax

--- arrayScripts/actors.js
+++ arrayScripts/actors.js
@@ -21,8 +21,8 @@ let academyMembers = [
   },
 ];
 //* Who is the Academy Member whose ID is 187?
-for (let i = 0; i < academyMembers.length; i++) {
-  if (academyMembers[i].memID === 187) {
+for(let i = 0; i < academyMembers.length; i++){
+  if(academyMembers[i].memID === 187){
     console.log(
       `The Academy member with the ID 187 is: ${academyMembers[i].name}.`
     );
@@ -30,8 +30,8 @@ for (let i = 0; i < academyMembers.length; i++) {
 }
 console.log();
 //* Who has have been in at least 3 films?
-for (let i = 0; i < academyMembers.length; i++) {
-  if (academyMembers[i].films.length >= 3) {
+for(let i = 0; i < academyMembers.length; i++){
+  if(academyMembers[i].films.length >= 3){
     console.log(
       `Academy member ${academyMembers[i].name} has been in at least 3 films.`
     );
@@ -39,8 +39,8 @@ for (let i = 0; i < academyMembers.length; i++) {
 }
 console.log();
 //* Who has a name that starts with "Bob"?
-for (let i = 0; i < academyMembers.length; i++) {
-  if (academyMembers[i].name.startsWith("Bob")) {
+for(let i = 0; i < academyMembers.length; i++){
+  if(academyMembers[i].name.startsWith("Bob")){
     console.log(
       `The Academy member with the name starting with "Bob" is: ${academyMembers[i].name}.`
     );
@@ -48,9 +48,9 @@ for (let i = 0; i < academyMembers.length; i++) {
 }
 console.log();
 //* HARDER: Which Academy Members have been in a film that starts with "A"
-for (let i = 0; i < academyMembers.length; i++) {
-  for (let j = 0; j < academyMembers[i].films.length; j++) {
-    if (academyMembers[i].films[j].startsWith("A")) {
+for(let i = 0; i < academyMembers.length; i++){
+  for(let j = 0; j < academyMembers[i].films.length; j++){
+    if(academyMembers[i].films[j].startsWith("A")){
       console.log(
         `Academy member ${academyMembers[i].name} has been in a film that starts with "A".`
       );
